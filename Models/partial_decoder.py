9ec26aab4225630b785c580017c3466245fbf226
Zakriya Paracha <82748498+ZakriyaParacha46@users.noreply.github.com>
2023-12-30 00:54:19 +0500

    Update partial_decoder.py
    There is no lib directory, hence we dont need to import "lib.conv_layer"

--- Models/partial_decoder.py
+++ Models/partial_decoder.py
@@ -7,7 +7,7 @@ Created on Tue Aug 10 17:20:30 2021
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from lib.conv_layer import Conv, BNPReLU
+from .conv_layer import Conv, BNPReLU
 import math
 
 class aggregation(nn.Module):
